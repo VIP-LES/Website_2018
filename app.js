a15aa57cefe2fcb887cafe4e37d02fe00dc25cb1
Kenny Shaw <kshaw@gatech.edu>
2018-03-10 15:35:43 -0500

    Put old Website HTML in handlebars temporarily while working on relearning Angular

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressHandlebars  = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,12 +9,13 @@ var stylus = require('stylus');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +47,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
 module.exports = app;
